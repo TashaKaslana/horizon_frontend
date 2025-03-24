2b66065de11cd8283e45b6d50166a1d9e3d36713
Phong <sanhok2104@gmail.com>
2025-03-24 20:31:12 +0700

    add app preferences global

--- src/app/(home)/foryou/video-section/VideoContainer.tsx
+++ src/app/(home)/foryou/video-section/VideoContainer.tsx
@@ -20,15 +20,20 @@ const VideoSection = () => {
     const {videoSettings} = useConfigStore()
 
     const getValueByKey = (key: string) => {
-        return videoSettings.find((v) => v.key === key)?.value ?? true
-    }
+        const value = videoSettings.find((v) => v.key === key)?.value;
+
+        if (typeof value === 'boolean') return value;
+        if (typeof value === 'string') return value;
+
+        return true;
+    };
 
     return (
         <div className={'h-[22rem] rounded-xl transition aspect-video'}>
             <video
-                autoPlay={getValueByKey('auto_play')}
-                loop={getValueByKey('loop_video')}
-                controls={getValueByKey('control_video')}
+                autoPlay={getValueByKey('auto_play') === true}
+                loop={getValueByKey('loop_video') === true}
+                controls={getValueByKey('control_video') === true}
                 className={'object-cover w-full h-full rounded-xl'}
                 src={'https://www.w3schools.com/tags/mov_bbb.mp4'}
             />
