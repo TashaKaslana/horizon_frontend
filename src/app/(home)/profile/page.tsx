203d13a3947f1d365632e61be248cf0d551a804b
Phong <sanhok2104@gmail.com>
2025-04-15 18:33:33 +0700

    add profile editing functionality with form validation and phone input

---
+++ src/app/(home)/profile/page.tsx
@@ -0,0 +1,11 @@
+import ProfileEditForm from "@/app/(home)/profile/ProfileEditForm";
+
+const Page = () => {
+  return (
+      <>
+          <ProfileEditForm/>
+      </>
+  )
+}
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import LandingHeader from "@/app/components/landing_page/landing_header/LandingHeader";
 import LandingMain from "@/app/components/landing_page/landing_main/LandingMain";
-
 export default function Home() {
     return (
         <div className={'space-y-4 p-1'}>
