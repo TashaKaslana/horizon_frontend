bc52753adf675adca61d260e0c7adc43e075f8e8
Phong <sanhok2104@gmail.com>
2025-05-14 21:28:04 +0700

    feat: add some link in history(for redirect)

--- src/app/components/user_dialog/history/columns.tsx
+++ src/app/components/user_dialog/history/columns.tsx
@@ -9,9 +9,11 @@ import {
     DropdownMenuLabel, DropdownMenuSeparator,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { MoreHorizontal } from "lucide-react";
+import {Button} from "@/components/ui/button";
+import {MoreHorizontal} from "lucide-react";
 import {ActivityPart, History} from "@/types/History";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
 
 export const historyColumns: ColumnDef<History>[] = [
     {
@@ -23,7 +25,7 @@ export const historyColumns: ColumnDef<History>[] = [
     {
         id: "activity",
         header: ({column}) => (
-            <DataTableColumnHeader column={column} title={'Activity'} />
+            <DataTableColumnHeader column={column} title={'Activity'}/>
         ),
         cell: ({row}) => {
             return <div className="flex flex-wrap gap-1 items-center">{renderParts(row.original.parts)}</div>;
@@ -31,27 +33,40 @@ export const historyColumns: ColumnDef<History>[] = [
     },
     {
         id: "createdAt",
-        header: ({ column }) => (
-            <DataTableColumnHeader column={column} title={'Happened At'} />
+        header: ({column}) => (
+            <DataTableColumnHeader column={column} title={'Happened At'}/>
         ),
         accessorFn: (row) => new Date(row.createdAt).toLocaleString(),
     },
     {
         id: "actions",
-        cell: ({ row }) => {
+        cell: ({row}) => {
             const history = row.original;
             return <Action history={history}/>;
         },
     },
 ];
 
-const Action = ({history} : {history: History}) => {
+const Action = ({history}: { history: History }) => {
+    const router = useRouter();
+
+    const targetPart = history.parts.find(
+        (part): part is Extract<ActivityPart, { type: 'object' }> => part.type === 'object'
+    );
+
+    const handleViewTargetPlace = () => {
+        if (targetPart) {
+            const path = switchLink(targetPart);
+            router.push(path);
+        }
+    };
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="h-8 w-8 p-0">
                     <span className="sr-only">Open menu</span>
-                    <MoreHorizontal className="h-4 w-4" />
+                    <MoreHorizontal className="h-4 w-4"/>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
@@ -61,8 +76,8 @@ const Action = ({history} : {history: History}) => {
                 >
                     Copy ID
                 </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>View target place</DropdownMenuItem>
+                <DropdownMenuSeparator/>
+                {targetPart && <DropdownMenuItem onClick={handleViewTargetPlace}>View target place</DropdownMenuItem>}
                 <DropdownMenuItem>View target details</DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
@@ -77,12 +92,30 @@ const renderParts = (parts: ActivityPart[]) => {
 
         if (part.type === 'object') {
             return (
-                <span key={idx} className="font-medium text-blue-600">
-          {part.label}
-        </span>
+                <Link key={idx} href={switchLink(part)}>
+                    <span className="font-medium text-blue-600 hover:underline">
+                        {part.label}
+                    </span>
+                </Link>
             );
         }
 
+
         return null;
     });
+};
+
+
+const switchLink = (part: Extract<ActivityPart, { type: 'object' }>) => {
+    switch (part.entity) {
+        case 'user':
+            return `/users/${part.id}/overview`;
+        case 'post':
+            return `/foryou/${part.id}`;
+        case 'comment':
+            return `/foryou/comments/${part.id}`;
+
+        default:
+            return '#';
+    }
 };
--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -34,8 +34,7 @@ export function DataTable<TData, TValue>({
                                              data,
                                              fetchNextPage,
                                              hasNextPage,
-                                             isLoading,
-                                             totalPageCount
+                                             isLoading
                                          }: DataTableProps<TData, TValue>) {
     const [sorting, setSorting] = React.useState<SortingState>([])
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
