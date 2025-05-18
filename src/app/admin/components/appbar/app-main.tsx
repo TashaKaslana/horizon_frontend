c2530f4536d039eb4f8c3a889981a9c4ead255cc
Phong <sanhok2104@gmail.com>
2025-05-18 19:40:18 +0700

    feat: add nav navigation link

--- src/app/admin/components/appbar/app-main.tsx
+++ src/app/admin/components/appbar/app-main.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { MailIcon, PlusCircleIcon, type LucideIcon } from "lucide-react"
+import {MailIcon, PlusCircleIcon, type LucideIcon} from "lucide-react"
 
-import { Button } from "@/components/ui/button"
+import {Button} from "@/components/ui/button"
 import {
     SidebarGroup,
     SidebarGroupContent,
@@ -10,6 +10,7 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import Link from "next/link";
 
 export function NavMain({
                             items,
@@ -29,7 +30,7 @@ export function NavMain({
                             tooltip="Quick Create"
                             className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
                         >
-                            <PlusCircleIcon />
+                            <PlusCircleIcon/>
                             <span>Quick Create</span>
                         </SidebarMenuButton>
                         <Button
@@ -37,19 +38,21 @@ export function NavMain({
                             className="h-9 w-9 shrink-0 group-data-[collapsible=icon]:opacity-0"
                             variant="outline"
                         >
-                            <MailIcon />
+                            <MailIcon/>
                             <span className="sr-only">Inbox</span>
                         </Button>
                     </SidebarMenuItem>
                 </SidebarMenu>
                 <SidebarMenu>
                     {items.map((item) => (
-                        <SidebarMenuItem key={item.title}>
-                            <SidebarMenuButton tooltip={item.title}>
-                                {item.icon && <item.icon />}
-                                <span>{item.title}</span>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
+                        <Link key={item.title} href={item.url}>
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton tooltip={item.title}>
+                                    {item.icon && <item.icon/>}
+                                    <span>{item.title}</span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </Link>
                     ))}
                 </SidebarMenu>
             </SidebarGroupContent>
