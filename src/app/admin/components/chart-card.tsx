a1fc8b5791503b271132b8016bb73e242d840dc5
Phong <sanhok2104@gmail.com>
2025-06-03 12:03:25 +0700

    feat: enhance chart components with improved styling and add tooltip customization

--- src/app/admin/components/chart-card.tsx
+++ src/app/admin/components/chart-card.tsx
@@ -23,8 +23,9 @@ import {
     ChartTooltipContent,
 } from "@/components/ui/chart"
 import {Spinner} from "@/components/ui/spinner"
+import {cn} from "@/lib/utils";
 
-type NormalizedChartData = { date: string } & Record<string, number>
+export type NormalizedChartData = { date: string } & Record<string, number>
 
 interface ChartCardProps {
     data?: NormalizedChartData[]
@@ -32,15 +33,17 @@ interface ChartCardProps {
     title: string
     description: string
     chartConfig: ChartConfig
+    tooltipClassName?: string
 }
 
 export function ChartCard({
-                                                         data,
-                                                         isLoading,
-                                                         title,
-                                                         description,
-                                                         chartConfig,
-                                                     }: ChartCardProps) {
+                              data,
+                              isLoading,
+                              title,
+                              description,
+                              chartConfig,
+                              tooltipClassName
+                          }: ChartCardProps) {
     return (
         <Card className="mx-4">
             <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
@@ -51,19 +54,23 @@ export function ChartCard({
             </CardHeader>
             <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6 h-[250px]">
                 {isLoading ? (
-                    <Spinner show={isLoading} />
+                    <Spinner show={isLoading}/>
+                ) : !data || data.length === 0 ? (
+                    <div className="flex h-full items-center justify-center text-muted-foreground">
+                        No data available
+                    </div>
                 ) : (
                     <ChartContainer config={chartConfig} className="aspect-auto h-[250px] w-full">
                         <AreaChart data={data}>
                             <defs>
                                 {Object.entries(chartConfig).map(([key, config]) => (
                                     <linearGradient id={key} key={key} x1="0" y1="0" x2="0" y2="1">
-                                        <stop offset="5%" stopColor={config.color} stopOpacity={0.8} />
-                                        <stop offset="95%" stopColor={config.color} stopOpacity={0.1} />
+                                        <stop offset="5%" stopColor={config.color} stopOpacity={0.8}/>
+                                        <stop offset="95%" stopColor={config.color} stopOpacity={0.1}/>
                                     </linearGradient>
                                 ))}
                             </defs>
-                            <CartesianGrid vertical={false} />
+                            <CartesianGrid vertical={false}/>
                             <XAxis
                                 dataKey="date"
                                 tickLine={false}
@@ -89,7 +96,7 @@ export function ChartCard({
                                             })
                                         }}
                                         indicator="dot"
-                                        className="w-36"
+                                        className={cn('w-fit', tooltipClassName)}
                                     />
                                 }
                             />
@@ -103,7 +110,7 @@ export function ChartCard({
                                     stackId="a"
                                 />
                             ))}
-                            <ChartLegend content={<ChartLegendContent />} />
+                            <ChartLegend content={<ChartLegendContent/>}/>
                         </AreaChart>
                     </ChartContainer>
                 )}
