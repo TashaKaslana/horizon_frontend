06e78f9c371ad7eddfa03daef84db6ed0a949f08
Nguyễn Tấn Phong <114403602+TashaKaslana@users.noreply.github.com>
2025-06-19 12:57:05 +0700

    Localize admin appbar and status page

--- src/app/admin/system/status/status-container.tsx
+++ src/app/admin/system/status/status-container.tsx
@@ -2,10 +2,13 @@
 
 import {SiteHeader} from "@/app/admin/components/site-header";
 import { StatusContainer } from "./components/status-container";
+import {useTranslations} from "next-intl";
 
 export function AdminStatusContainer() {
+    const t = useTranslations('Admin.system.status');
+
     return <div className={"h-screen"}>
-        <SiteHeader text={"System Status"}/>
+        <SiteHeader text={t('title')}/>
         <StatusContainer/>
     </div>;
 }
